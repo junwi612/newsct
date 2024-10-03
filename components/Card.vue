a9d994d9fc52d6c225c85fcdebb91f1d4b0084ec
Jun <jun.we@outlook.com>
2024-10-03 18:35:46 +0800

    style: 更新元素显示内容

--- components/Card.vue
+++ components/Card.vue
@@ -3,12 +3,12 @@
     <view class="px-4 sm:px-0 flex flex-col lg:flex-row">
       <view class="flex-1 w-full mb-5">
         <view
-          class="bg-white overflow-hidden shadow lg:h-96 rounded-lg flex flex-col"
+          class="bg-white overflow-hidden shadow rounded-lg flex flex-col"
           @click="goToDetail(1)"
         >
-          <image
-            class="object-cover rounded-t-lg w-full"
-            src="https://picsum.photos/id/1018/800/450"
+          <img
+            class="object-cover rounded-t-lg max-h-96 lg:max-h-96 h-auto"
+            src="https://fastly.picsum.photos/id/866/1920/1080.jpg?hmac=dNBuPEp10RySTqlc5EpGw7QyrFIpBd2X88r1Ixla7pw"
             alt="新闻图片"
           />
           <view class="p-4 flex-1">
