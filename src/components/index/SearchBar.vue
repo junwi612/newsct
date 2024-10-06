0a850b617697d3b006d76c00b7ee9e34405409b3
Jun <jun.we@outlook.com>
2024-10-06 12:21:15 +0800

    chore: Initialization Migration to Vue

---
+++ src/components/index/SearchBar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div class="px-4 hidden lg:block sm:px-0 mb-6">
+      <uni-search-bar
+        bgColor="#fff"
+        class="uni-mt-10"
+        radius="5"
+        placeholder="搜索内容"
+        clearButton="auto"
+        cancelButton="none"
+        @confirm="search"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
